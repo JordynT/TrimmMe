76c35c9a38d55535f52fd8f56949215efbfc080a
Jordyn Tacoronte <jordyntacoronte@gmail.com>
2015-03-31 15:22:48 -0700

    fixed actual plan

--- soloproject/app/Http/Controllers/DashboardController.php
+++ soloproject/app/Http/Controllers/DashboardController.php
@@ -25,15 +25,12 @@ class DashboardController Extends Controller {
 
 		$num_days = $plan->num_days;
 		$c_start_date= new Carbon($start_date);
-		//dd($c_start_date);
 		$plan_adjustment = $plan->getAdjustment();
 
 		if($plan_adjustment) {
 			$adjust_date = new Carbon($plan_adjustment->adjustment_date);
-			//dd($adjust_date);
 			$num_days = $plan_adjustment->num_days;
-			//$adjusted_pounds_day = 
-			//dd($num_days);	 
+	 
 		} 
 
 		$checkin_array = $this->getCheckin($plan_id, $rmr);
@@ -73,23 +70,6 @@ class DashboardController Extends Controller {
 				$goal_pounds = $plan->lose_weight / $plan->num_days;
 			}
 
-			// if($plan_adjustment) {
-			// 	$di = $adjust_date->diffInDays($c_start_date);
-			// 	$dr = $plan_adjustment->num_days - $di;
-			// 	$pi = $di * $goal_pounds;
-			// 	$pr = $plan_adjustment->end_weight - $pi;
-			// 	$flux = $pr/ $dr;
-			// 	$goal_pounds  $flux;
-				//dd($goal_pounds);
-
-				//dd($pr);
-				//if($di->invert == 0) {
-					//echo 'update graph for adjustment';
-					//$goal_pounds += $plan_adjustment->end_weight/ $plan_adjustment->num_days;
-				//}
-			// }
-			
-
 			// Create a new entry for daily
 			$day_item = [
 						'day' => $day,
@@ -98,7 +78,6 @@ class DashboardController Extends Controller {
 						];
 
 			$cumGoalPounds = $cumGoalPounds + $goal_pounds;
-			//dd($cumGoalPounds);
 			$cumActualPounds = $cumActualPounds + $this->getActualPounds($this_date, $checkin_array);
 
 			$Cummulitive_day = [
@@ -106,14 +85,12 @@ class DashboardController Extends Controller {
 						'goal_pounds' => $cumGoalPounds,
 						'actual_pounds' => $cumActualPounds
 			];
-			//dd($Cummulitive_day);
 			array_push($daily_array, $day_item);
 			array_push($Cummulitive, $Cummulitive_day);
 
 		}
-		//dd($daily_array);
 
-		return view('dashboard', ['Cummulitive' => $Cummulitive, 'plan' => $plan]);
+		return view('dashboard', ['Cummulitive' => $Cummulitive, 'plan' => $plan, 'plan_adjustment' => $plan_adjustment ]);
 	}
 
 
--- soloproject/resources/views/dashboard.blade.php
+++ soloproject/resources/views/dashboard.blade.php
@@ -48,7 +48,7 @@
 <div class="information">
 	<div class="info">
 		<span class="actual">Actual Plan:</span>
-		<p class="plan-info">You are planned to lose <strong>5 pounds</strong> in <strong>21 days</strong> which is <strong>under</strong> your plan. </p>
+		<p class="plan-info">At your current daily rate, you will reach your goal of losing <strong> @if($plan_adjustment){{$plan_adjustment->end_weight}} @else {{$plan->lose_weight}} @endif pounds</strong> in a total of <strong>21 days</strong>, which is <strong>under</strong> your plan. </p>
 	</div>
 	<div class="info">
 		<span class="original">Original Plan:</span>
